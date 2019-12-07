ba37171a9855123ff631d89882859719e25bb971
hazam <hazam@yandex-team.ru>
2019-12-07 18:12:43 +0500

    Day 7. Part 2. Haskell

--- haskell/src/Day7.hs
+++ haskell/src/Day7.hs
@@ -7,6 +7,7 @@ where
 import           Lib
 import           IntcodeComputer
 import           Data.List
+import           Debug.Trace
 
 prepareInput = do
     registries <- map (read :: String -> Int) <$> readInputForDayAsCSV 7
@@ -19,16 +20,57 @@ prepareInput = do
 
 part1 = findMaxOutput <$> prepareInput
 
-part2 = putStrLn "ALOHA"
+part2 = findMaxOutput' <$> prepareInput
 
-runAmplifiers :: Program -> [Int] -> Program
-runAmplifiers p (ph1 : phs) =
-    let programA = runProgramm $ p { input = ph1 : [0] }
+runAmplifiersWithInit :: Int -> Program -> [Int] -> Program
+runAmplifiersWithInit init p (ph1 : phs) =
+    let programA = runProgramm $ p { input = ph1 : [init] }
         reducer acc ph = runProgramm p { input = ph : [last (output acc)] }
     in  foldl reducer programA phs
 
+runAmplifiers :: Program -> [Int] -> Int
+runAmplifiers p phs = last . output $ runAmplifiersWithInit 0 p phs
+
 findMaxOutput :: Program -> Int
-findMaxOutput p = maximum
-    [ (last . output) $ runAmplifiers p phase
-    | phase <- permutations [0, 1, 2, 3, 4]
+findMaxOutput p =
+    maximum [ runAmplifiers p phase | phase <- permutations [0, 1, 2, 3, 4] ]
+
+newtype Amplifiers = Amps (Program, Program, Program, Program, Program) deriving Show
+
+iterAmplifiersInFeedbackLoop :: Amplifiers -> Amplifiers
+iterAmplifiersInFeedbackLoop amps@(Amps (p1, p2, p3, p4, p5)) =
+    let inputFromOutput p = [ last . output $ p | (not . null . output) p ]
+        p2' = runProgramm $ p2 { input = inputFromOutput p1 }
+        p3' = runProgramm $ p3 { input = inputFromOutput p2' }
+        p4' = runProgramm $ p4 { input = inputFromOutput p3' }
+        p5' = runProgramm $ p5 { input = inputFromOutput p4' }
+        p1' = runProgramm $ p1 { input = inputFromOutput p5' }
+    in  Amps (p1', p2', p3', p4', p5')
+
+runAmplifiersInFeedbackLoop :: [Int] -> Program -> Int
+runAmplifiersInFeedbackLoop phases =
+    last
+        . output
+        . lastAmp
+        . head
+        . dropWhile ((/= Halted) . status . lastAmp)
+        . iterate iterAmplifiersInFeedbackLoop
+        . initAmps phases
+
+lastAmp :: Amplifiers -> Program
+lastAmp (Amps (_, _, _, _, p)) = p
+
+initAmps :: [Int] -> Program -> Amplifiers
+initAmps (ph1 : ph2 : ph3 : ph4 : ph5 : _) p = Amps
+    ( runProgramm $ p { input = [ph1, 0] }
+    , runProgramm $ p { input = [ph2] }
+    , runProgramm $ p { input = [ph3] }
+    , runProgramm $ p { input = [ph4] }
+    , runProgramm $ p { input = [ph5] }
+    )
+
+findMaxOutput' :: Program -> Int
+findMaxOutput' p = maximum
+    [ runAmplifiersInFeedbackLoop phase p
+    | phase <- permutations [5, 6, 7, 8, 9]
     ]
--- haskell/src/IntcodeComputer.hs
+++ haskell/src/IntcodeComputer.hs
@@ -8,7 +8,7 @@ where
 import           Lib
 
 
-data ProgramStatus = Running | Halted deriving Show
+data ProgramStatus = Running | Halted | WaitingForInput deriving (Show, Eq)
 
 data Program = Program {
     registries :: [Int],
@@ -35,41 +35,55 @@ runProgramm program =
     let command  = parseCommand program
         program' = runCommand program command
     in  case status program' of
-            Halted  -> program'
-            Running -> runProgramm program'
+            Halted          -> program'
+            WaitingForInput -> program'
+            Running         -> runProgramm program'
 
 runCommand :: Program -> Command -> Program
 runCommand program command = case command of
     (SumC l r p) -> program
         { registries = replaceNth p (l + r) (registries program)
         , position   = position program + 4
+        , status     = Running
         }
     (ProdC l r p) -> program
         { registries = replaceNth p (l * r) (registries program)
         , position   = position program + 4
+        , status     = Running
         }
-    (InputC p) -> program
-        { registries = replaceNth p (head $ input program) $ registries program
-        , input      = tail $ input program
-        , position   = position program + 2
-        }
+    (InputC p) -> case input program of
+        [] -> program { status = WaitingForInput }
+        _  -> program
+            { registries = replaceNth p (head $ input program)
+                               $ registries program
+            , input      = tail $ input program
+            , position   = position program + 2
+            , status     = Running
+            }
     (OutputC p) -> program
         { output   = output program ++ [registries program !! p]
         , position = position program + 2
+        , status   = Running
         }
-    (JumpIfTrueC l p) ->
-        program { position = if l == 0 then position program + 3 else p }
-    (JumpIfFalseC l p) ->
-        program { position = if l == 0 then p else position program + 3 }
+    (JumpIfTrueC l p) -> program
+        { position = if l == 0 then position program + 3 else p
+        , status   = Running
+        }
+    (JumpIfFalseC l p) -> program
+        { position = if l == 0 then p else position program + 3
+        , status   = Running
+        }
     (LessThanC l r p) -> program
         { registries = replaceNth p (if l < r then 1 else 0)
                            $ registries program
         , position   = position program + 4
+        , status     = Running
         }
     (EqualsC l r p) -> program
         { registries = replaceNth p (if l == r then 1 else 0)
                            $ registries program
         , position   = position program + 4
+        , status     = Running
         }
     Halt -> program { status = Halted }
 
